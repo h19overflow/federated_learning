203e5435c400b811fcc169b21d578d3c338e9d34
h19overflow <hamzakhaledlklk@gmail.com>
2025-10-11 19:19:05 +0800

    chore: remove deprecated local settings file; streamline configuration management

---
+++ federated_pneumonia_detection/src/control/federated_learning/server_app.py
@@ -0,0 +1,36 @@
+import torch
+from flwr.server import ServerApp
+from flwr.server.strategy import FedAvg
+from flwr.common import Context, ArrayRecord, ConfigRecord
+from flower_tutorial.task import Net
+
+# Create ServerApp instance
+app = ServerApp()
+
+@app.main()
+def main(grid, context: Context) -> None:
+    """Main entry point for the ServerApp."""
+    # Read run config
+    fraction_train: float = context.run_config["fraction-train"]
+    num_rounds: int = context.run_config["num-server-rounds"]
+    lr: float = context.run_config["lr"]
+
+    # Load global model
+    global_model = Net()
+    arrays = ArrayRecord(global_model.state_dict())
+
+    # Initialize FedAvg strategy
+    strategy = FedAvg(fraction_train=fraction_train)
+
+    # Start strategy, run FedAvg for `num_rounds`
+    result = strategy.start(
+        grid=grid,
+        initial_arrays=arrays,
+        train_config=ConfigRecord({"lr": lr}),
+        num_rounds=num_rounds,
+    )
+
+    # Save final model to disk
+    print("\nSaving final model to disk...")
+    state_dict = result.arrays.to_torch_state_dict()
+    torch.save(state_dict, "final_model.pt")
